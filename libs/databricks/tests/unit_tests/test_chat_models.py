69c7918adc2457c61d4a1c49c2c11cf04d14b7ca
B-Step62 <yuki.watanabe@databricks.com>
2024-09-18 15:16:26 +0900

    Support LangChain 0.3.0
    Signed-off-by: B-Step62 <yuki.watanabe@databricks.com>

--- libs/databricks/tests/unit_tests/test_chat_models.py
+++ libs/databricks/tests/unit_tests/test_chat_models.py
@@ -20,7 +20,7 @@ from langchain_core.messages import (
     ToolMessageChunk,
 )
 from langchain_core.messages.tool import ToolCallChunk
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 
 from langchain_databricks.chat_models import (
     ChatDatabricks,
